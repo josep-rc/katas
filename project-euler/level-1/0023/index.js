f885357e14b2ccdfe88077daaf6d986a65dabb74
Josep Peñalba <jopema7@gmail.com>
2022-10-12 12:43:36 +0200

    lvl1ex23

--- project-euler/level-1/0023/index.js
+++ project-euler/level-1/0023/index.js
@@ -1,7 +1,7 @@
 console.time("time")
 
 
-
+// return an array with divisors of n
 function divisors(n){
     const divs=[1]
     for (let i = 2; i < (n/i)+1; i++) {
@@ -11,10 +11,10 @@ function divisors(n){
             if(i!=(n/i)) divs.push(n/i)
         }
     }
-    return divs
-    // return divs.sort((a,b)=>a-b)
+    return divs.sort((a,b)=>a-b)
 }
 
+// return de sum of divisors of n
 function sumDivisors(n){
     const divs = divisors(n)
     let sum = 0
@@ -24,23 +24,69 @@ function sumDivisors(n){
     return sum
 }
 
-
-let double=0
-let i=1
-let sumNotDoubleAbundants = 0
-while (i<28200) { // 28123
-    if(i<sumDivisors(i)){
-        // es abundante
-        double=i*2
-        console.log(i, double)
-    }else{
-        console.log(i)
-        sumNotDoubleAbundants+=i
+// return abundant numbers (sum of divisors bigger than the number) until max
+function findAbundantsTo(max){
+    let i=1
+    const abundants = []
+    while(i<max){
+        if(i<sumDivisors(i)) abundants.push(i)
+        i++
     }
-    i++
+    return abundants
 }
 
-console.log(sumNotDoubleAbundants)
+// return an array with all posibles pair sums results of list
+function combineSumPairs(list){
+    let sums = []
+    let sum = 0
+    for (let i = 0; i < list.length; i++) {
+        for (let y = i; y < list.length; y++) {
+            sum=list[i]+list[y]
+            sums.push(sum)
+        }
+    }
+    // order
+    sums.sort((a,b)=>a-b)
+    // remove duplicates
+    const noDuplicates = []
+    for (let i = 0; i < sums.length; i++) {
+        if(sums[i+1]!==sums[i]){
+            noDuplicates.push(sums[i])
+        }
+    }
+    return noDuplicates
+}
+
+// return an array of missing numbers of list until max
+function findMissingNumbers(list, max){
+    const missing = []
+    let index = 0
+    for (let i = 1; i <= max; i++) {
+        if(i<list[index]){
+            missing.push(i)
+        }else{
+            index++
+        }
+    }
+    return missing
+}
+
+// sum numbers of list
+function sumArray(list){
+    let sum=0
+    for (let i = 0; i < list.length; i++) {
+        sum+= list[i];
+    }
+    return sum
+}
+
+const abundants = findAbundantsTo(28123)
+const sums = combineSumPairs(abundants)
+const notInSums = findMissingNumbers(sums, 28200)
+console.log(sumArray(notInSums))
 
 
 console.timeEnd("time")
+
+// 4179871
+// time: 3.355s
