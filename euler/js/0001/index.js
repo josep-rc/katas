33fa2c16aa790e8c04c67f16694d1a5fff4a39da
Josep Peñalba <jopema7@gmail.com>
2022-10-17 23:22:05 +0200

    Prettified

--- euler/js/0001/index.js
+++ euler/js/0001/index.js
@@ -1,14 +1,13 @@
-"use strict"
+"use strict";
 
-console.time("test")
+console.time("test");
 
-let sum = 0
+let sum = 0;
 
 for (var i = 3; i < 1000; i++) {
-    if (i % 3 == 0 || i % 5 == 0) sum += i
+  if (i % 3 == 0 || i % 5 == 0) sum += i;
 }
 
-console.log(sum)
+console.log(sum);
 
-console.timeEnd("test")
-
+console.timeEnd("test");
